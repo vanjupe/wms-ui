f89e7577221d5e3d164097bd6652ab16a9d69cdb
Rocio Bautista Romero <49208524+rociobautistaromero04@users.noreply.github.com>
2021-04-27 16:50:45 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import route from '@/route';
+
+import GlobalComponents from './global/GlobalComponents'
 
 Vue.config.productionTip = false;
+const router = route.getRouter(Vue)
+GlobalComponents.load(Vue)
+
 
 new Vue({
   vuetify,
+  router,
   render: (h) => h(App),
 }).$mount("#app");
