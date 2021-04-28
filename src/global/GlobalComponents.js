67c711a208cdfc821ee9e5ba09be99fcde43b743
Rocio Bautista Romero <49208524+rociobautistaromero04@users.noreply.github.com>
2021-04-28 16:33:52 -0500

    component dialogo

--- src/global/GlobalComponents.js
+++ src/global/GlobalComponents.js
@@ -1,7 +1,9 @@
 import Wrapper from './../components/General/Wrapper'
+import Dialogo from '../components/General/Dialogo'
 
 let load = (vue) => {
     vue.component('Wrapper', Wrapper)
+    vue.component('Dialogo', Dialogo)
 }
 
 export default { load }
