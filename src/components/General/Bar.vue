6b7e82ad21af1496a2d6791baf7ae5cff71211a8
Rocio Bautista Romero <49208524+rociobautistaromero04@users.noreply.github.com>
2021-04-28 16:32:48 -0500

    Create Bar.vue

---
+++ src/components/General/Bar.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app-bar app>
+        <v-app-bar-nav-icon v-if="$vuetify.breakpoint.smAndDown" @click.stop="$navegador = !$navegador"/>
+        <v-tabs v-else grow show-arrows>
+            <v-tab v-for="(opcion, index) in $menu" :key="index" active-class="menu-seleccionado" :to="opcion.to">
+                {{ opcion.texto }}
+            </v-tab>
+        </v-tabs>
+        <v-spacer/>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        name: "Bar"
+    }
+</script>
+
+<style scoped>
+
+</style>
