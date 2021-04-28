1f023c07ff51d1242540504d1f96c7899ac71a86
Rocio Bautista Romero <49208524+rociobautistaromero04@users.noreply.github.com>
2021-04-28 16:33:59 -0500

    Update Almacenes.vue

--- src/components/Almacenes/Almacenes.vue
+++ src/components/Almacenes/Almacenes.vue
@@ -1,17 +1,17 @@
 <template>
     <wrapper titulo="Almacénes">
-        <v-data-table  :headers="headers" :items="desserts"/>
+        <v-data-table  :headers="headers" :items="almacenes"/>
         <dialogo v-model="dialogAgregarAlmacen" title="Agregar Almacén">
             <v-row>
                 <v-col cols="12" sm="12">
-                    <v-text-field label="Nombre de almacén" autofocus/>
+                    <v-text-field v-model="almacenAOperar.nombre" label="Nombre de almacén" autofocus/>
                 </v-col>
                 <v-col cols="12" sm="12">
-                    <v-text-field label="Clave"/>
+                    <v-text-field v-model="almacenAOperar.clave" label="Clave"/>
                 </v-col>
             </v-row>
             <template slot="actions">
-                <v-btn text color="primary" @click="dialogAgregarAlmacen = false">Agregar</v-btn>
+                <v-btn text color="primary" @click="agregarAlmacen">Agregar</v-btn>
             </template>
         </dialogo>
         <v-btn color="success"  dark fab fixed bottom right @click="dialogAgregarAlmacen = true">
@@ -21,8 +21,42 @@
 </template>
 
 <script>
+    import AlmacenServices from "../../services/AlmacenServices";
+
     export default {
-        name: "Almacenes"
+        name: "Almacenes",
+        data() {
+            return{
+                headers: [
+                    { text: 'Nombre', value: 'nombre' },
+                    { text: 'Clave', value: 'clave' }
+                ],
+                dialogAgregarAlmacen: false,
+                almacenes: [],
+                almacenAOperar: {}
+            }
+        },
+        async created () {
+            this.$loader = true
+            try {
+                this.almacenes = await AlmacenServices.getAlmacenes()
+            } catch (e) {
+                this.$loader = false
+                console.log(e)
+            }
+            this.$loader = false
+        },
+        methods:{
+            async agregarAlmacen(){
+                this.$loader = true
+                try {
+                   this.almacenes = await AlmacenServices.postGuardarAlmacen(this.almacenAOperar)
+                } catch (e) {
+                    console.log(e)
+                }
+
+            }
+        }
     }
 </script>
 
