d10bfe063e9ef2232056a987475370046533bf86
Rocio Bautista Romero <49208524+rociobautistaromero04@users.noreply.github.com>
2021-05-13 14:50:08 -0500

    formulario de zonas

--- src/components/Zonas/Zonas.vue
+++ src/components/Zonas/Zonas.vue
@@ -1,27 +1,29 @@
 <template>
-    <wrapper titulo="Zonas">
-        <v-data-table  :headers="headers" :items="zonas"/>
-        <dialogo v-model="dialogAgregarZona" title="Agregar zona">
+    <wrapper>
+        <v-data-table  :headers="headers" :items="zonas">
+            <template v-slot:item.acciones="{ item }">
+                <v-btn icon class="primary--text" @click="abrirDialogZona(item)"><v-icon small>mdi-pencil</v-icon></v-btn>
+            </template>
+        </v-data-table>
+        <dialogo v-model="dialogOperarZona" :title="zonaAOperar.id ? 'Editar Zona' : 'Agregar Zona'">
             <v-row>
                 <v-col cols="12" sm="12">
                     <v-text-field v-model="zonaAOperar.nombre" label="Nombre" autofocus/>
                 </v-col>
                 <v-col cols="12" sm="12">
-                    <v-text-field v-model="zonaAOperar.numero" label="Número"/>
+                    <v-select v-model="zonaAOperar.idCliente" label="Selecciona el cliente" :items="clientes"
+                              item-value="id" item-text="nombre"/>
                 </v-col>
                 <v-col cols="12" sm="12">
-                    <v-select v-model="zonaAOperar.cliente"
-                              label="Selecciona el colegio" :items="colegios"/>
-                </v-col>
-                <v-col cols="12" sm="12">
-                    <v-select v-model="zonaAOperar.almacen" label="Selecciona el almacén" :items="almacenes"/>
+                    <v-select v-model="zonaAOperar.idAlmacen" label="Selecciona el almacén" :items="almacenes"
+                              item-value="id" item-text="nombre"/>
                 </v-col>
             </v-row>
             <template slot="actions">
-                <v-btn text color="primary" @click="agregarZona">Agregar</v-btn>
+                <v-btn text color="primary" @click="zonaAOperar.id ? editarZona() : agregarZona()">Guardar</v-btn>
             </template>
         </dialogo>
-        <v-btn color="success"  dark fab fixed bottom right @click="dialogAgregarZona = true">
+        <v-btn color="success"  dark fab fixed bottom right @click="abrirDialogZona">
             <v-icon>mdi-plus</v-icon>
         </v-btn>
     </wrapper>
@@ -29,30 +31,24 @@
 
 <script>
     import ZonaServices from "../../services/ZonaServices";
+    import AlmacenServices from "../../services/AlmacenServices";
+    import ClienteServices from "../../services/ClienteServices";
 
     export default {
         name: "Zonas",
         data () {
             return{
-                dialogAgregarZona: false,
+                dialogOperarZona: false,
                 headers: [
                     { text: 'Nombre', value: 'nombre' },
-                    { text: 'Número', value: 'numero' },
-                    { text: 'Almacen', value: 'almacen' },
-                    { text: 'Cliente', value: 'cliente' },
+                    { text: 'Almacen', value: 'almacen.nombre' },
+                    { text: 'Cliente', value: 'cliente.nombre' },
+                    { text: '', value: 'acciones' }
                 ],
                 zonas: [],
                 zonaAOperar: {},
-                colegios: [
-                    'Centro Escolar Espejo de los Lirios',
-                    'Instituto Thomas Jefferson Sta. Mónica',
-                    'Estefanía Castañeda Colegio',
-                    'Robert F. Kennedy Colegio (Atizapán)',
-                    'María Montessori Colegio C.T.M.'],
-                almacenes:[
-                    'Almacén interno',
-                    'Almacén externo'
-                ]
+                clientes: [],
+                almacenes: []
             }
         },
         async created () {
@@ -64,12 +60,46 @@
                 console.log(e)
             }
             this.$loader = false
+            let vm = this
+            AlmacenServices.getAlmacenes()
+                .then(almacenes => {
+                    vm.almacenes = almacenes
+                })
+            ClienteServices.getClientes()
+            .then(clientes => {
+                vm.clientes = clientes
+            })
         },
         methods:{
-            agregarZona(){
-                this.zonas.push(this.zonaAOperar)
-                this.dialogAgregarZona = false
-            }
+            async agregarZona() {
+                console.log(this.zonaAOperar)
+                this.$loader = true
+                try {
+                    let zonaAgregada = await ZonaServices.postGuardarZona(this.zonaAOperar)
+                    this.zonas.push(zonaAgregada)
+                } catch (e) {
+                    console.log(e)
+                }
+                this.$loader = false
+                this.dialogOperarZona = false
+            },
+            async editarZona() {
+                this.$loader = true
+                try {
+                    let zonaEditada = await ZonaServices.actualizarZona(this.zonaAOperar)
+                    let index = this.$getIndex(this.zonas, 'id', zonaEditada)
+                    this.$set(this.zonas, index, this.zonaAOperar)
+                } catch (e) {
+                    console.log(e)
+                }
+                this.$loader = false
+                this.dialogOperarZona = false
+            },
+            abrirDialogZona (zona) {
+                this.zonaAOperar = JSON.parse(JSON.stringify(zona))
+                if (this.$refs.form) this.$refs.form.resetValidation()
+                this.dialogOperarZona = true
+            },
         }
     }
 </script>
