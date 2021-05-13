e57ca03ba80fdb2ab690baaf5ca0f569b824c354
Rocio Bautista Romero <49208524+rociobautistaromero04@users.noreply.github.com>
2021-05-13 14:46:45 -0500

    métodos crud para almacenes

--- src/components/Almacenes/Almacenes.vue
+++ src/components/Almacenes/Almacenes.vue
@@ -1,20 +1,49 @@
 <template>
-    <wrapper titulo="Almacénes">
-        <v-data-table  :headers="headers" :items="almacenes"/>
-        <dialogo v-model="dialogAgregarAlmacen" title="Agregar Almacén">
+    <wrapper>
+        <v-data-table  :headers="headers" :items="almacenes">
+            <template v-slot:item.acciones="{ item }">
+                <v-btn icon @click="abrirDialogAlmacen(item)" class="primary--text"><v-icon small>mdi-pencil</v-icon></v-btn>
+            </template>
+        </v-data-table>
+        <dialogo v-model="dialogOperarAlmacen" :title="almacenAOperar.id ? 'Editar Almacén' : 'Agregar Almacén'">
             <v-row>
                 <v-col cols="12" sm="12">
                     <v-text-field v-model="almacenAOperar.nombre" label="Nombre de almacén" autofocus/>
                 </v-col>
                 <v-col cols="12" sm="12">
-                    <v-text-field v-model="almacenAOperar.clave" label="Clave" @keyup.enter="agregarAlmacen"/>
+                    <v-text-field v-model="almacenAOperar.direccion" label="Dirección" @keyup.enter="almacenAOperar.id ? editarAlmacen() : agregarAlmacen()"/>
                 </v-col>
             </v-row>
             <template slot="actions">
-                <v-btn text color="primary" @click="agregarAlmacen">Agregar</v-btn>
+                <v-btn text class="primary--text" @click="almacenAOperar.id ? editarAlmacen() : agregarAlmacen()">Guardar</v-btn>
             </template>
         </dialogo>
-        <v-btn color="success"  dark fab fixed bottom right @click="dialogAgregarAlmacen = true">
+        <dialogo v-model="dialogEliminarAlmacen" :title="'¿Desea eliminar ' + almacenAOperar.nombre + ' ?'">
+            <template slot="actions">
+                <v-btn text class="primary--text" @click="eliminarAlmacen()">Aceptar</v-btn>
+            </template>
+        </dialogo>
+        <v-dialog max-width="600" transition="dialog-top-transition">
+            <v-card style="border-radius: 15px">
+                <v-card-title></v-card-title>
+                <v-card-text>
+                    <v-row class="pa-9">
+                        <v-col cols="12" sm="12" class="text-sm-center">
+                            <v-icon class="red--text" size="50px">mdi-delete-alert</v-icon>
+                        </v-col>
+                        <v-col cols="12" sm="12" class="text-sm-center">
+                            <h2>¿Desea eliminar el registro?</h2>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer/>
+                    <v-btn text plain>Cancelar</v-btn>
+                    <v-btn text color="primary" plain loading>Aceptar</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        <v-btn color="success"  dark fab fixed bottom right @click="abrirDialogAlmacen">
             <v-icon>mdi-plus</v-icon>
         </v-btn>
     </wrapper>
@@ -29,11 +58,13 @@
             return{
                 headers: [
                     { text: 'Nombre', value: 'nombre' },
-                    { text: 'Clave', value: 'clave' }
+                    { text: 'Dirección', value: 'direccion' },
+                    { text: '', value: 'acciones' }
                 ],
-                dialogAgregarAlmacen: false,
+                dialogOperarAlmacen: false,
                 almacenes: [],
-                almacenAOperar: {}
+                almacenAOperar: {},
+                dialogEliminarAlmacen: false,
             }
         },
         async created () {
@@ -46,13 +77,55 @@
             }
             this.$loader = false
         },
-        methods:{
-            agregarAlmacen(){
-                this.almacenes.push(this.almacenAOperar)
-                this.dialogAgregarAlmacen = false
-            }
+        methods: {
+            async agregarAlmacen() {
+                this.$loader = true
+                try {
+                    let almacenAgregado = await AlmacenServices.postGuardarAlmacen(this.almacenAOperar)
+                    this.almacenes.push(almacenAgregado)
+                } catch (e) {
+                    console.log(e)
+                }
+                this.$loader = false
+                this.dialogOperarAlmacen = false
+            },
+            abrirDialogAlmacen (almacen = { tipo: 'almacen' }) {
+                this.almacenAOperar = JSON.parse(JSON.stringify(almacen))
+                if (this.$refs.form) this.$refs.form.resetValidation()
+                this.dialogOperarAlmacen = true
+            },
+            async editarAlmacen() {
+                this.$loader = true
+                try {
+                    let almacenEditado = await AlmacenServices.actualizarAlmacen(this.almacenAOperar)
+                    let index = this.$getIndex(this.almacenes, 'id', almacenEditado)
+                    this.$set(this.almacenes, index, almacenEditado)
+                } catch (e) {
+                    console.log(e)
+                }
+                this.$loader = false
+                this.dialogOperarAlmacen = false
+            },
+            abrirDialogEliminarAlmacen (almacen = { tipo: 'almacen' }) {
+                this.almacenAOperar = JSON.parse(JSON.stringify(almacen))
+                this.dialogEliminarAlmacen = true
+            },
+            async eliminarAlmacen() {
+                this.$loader = true
+                try {
+                    this.almacenAOperar.estatus = false
+                    let almacenEditado = await AlmacenServices.actualizarAlmacen(this.almacenAOperar)
+                    let index = this.$getIndex(this.almacenes, 'id', almacenEditado)
+                    this.$set(this.almacenes, index, almacenEditado)
+                } catch (e) {
+                    console.log(e)
+                }
+                this.$loader = false
+                this.dialogEliminarAlmacen = false
+            },
         }
     }
+
 </script>
 
 <style scoped>
