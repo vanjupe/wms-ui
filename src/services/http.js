7466971caeb4995052e6239e99882caec3a678f8
Rocio Bautista Romero <49208524+rociobautistaromero04@users.noreply.github.com>
2021-05-13 14:49:01 -0500

    put y remove

--- src/services/http.js
+++ src/services/http.js
@@ -32,9 +32,46 @@ let post = (url, data = {}) => {
             resolve(response.data)
         }).catch(function (error) {
             reject(error)
+            console.log(error)
+        })
+    })
+}
+
+let put = (url, data = {}) => {
+    return new Promise((resolve, reject) => {
+        axios({
+            method: 'put',
+            url,
+            data: JSON.stringify(data),
+            auth: visAuth,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (response) {
+            resolve(response.data)
+        }).catch(function (error) {
+            reject(error)
+            console.log(error)
+        })
+    })
+}
+
+let remove = (url) => {
+    return new Promise((resolve, reject) => {
+        axios({
+            method: 'delete',
+            url,
+            auth: visAuth,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (response) {
+            resolve(response.data)
+        }).catch(function (error) {
+            reject(error)
         })
     })
 }
 
 
-export default { get, post }
+export default { get, post, put, remove }
