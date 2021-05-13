0d0def16ec95bc683491d92d5c38b362cf6d7277
Rocio Bautista Romero <49208524+rociobautistaromero04@users.noreply.github.com>
2021-05-13 14:48:33 -0500

    ostGuardarUbicacion, actualizarUbicacion, eliminarUbicacion

--- src/services/UbicacionServices.js
+++ src/services/UbicacionServices.js
@@ -2,9 +2,23 @@ import AuthConst from '../config/AuthConst'
 import http from "./http";
 
 let getUbicaciones = () => {
-    let url = AuthConst.BASE_URL_WMS + '/ubicaciones/zona/1'
+    let url = AuthConst.BASE_URL_WMS + '/ubicaciones'
     return http.get(url)
 }
 
+let postGuardarUbicacion = (ubicacion) => {
+    let url = AuthConst.BASE_URL_WMS + '/ubicaciones'
+    return http.post(url, ubicacion)
+}
 
-export default { getUbicaciones }
+let actualizarUbicacion = (ubicacion, callback) => {
+    let url = AuthConst.BASE_URL_WMS + '/ubicaciones'
+    return http.put(url, ubicacion, callback)
+}
+
+let eliminarUbicacion = (idUbicacion, callback) => {
+    let url = AuthConst.BASE_URL_WMS + '/ubicaciones/'+ idUbicacion
+    return http.remove(url, callback)
+}
+
+export default { getUbicaciones, postGuardarUbicacion, actualizarUbicacion, eliminarUbicacion }
