0f5c1e415446e74deb9e84b26e861bfc5aafcfaa
Marco Antonio Andrade Viana <45572715+marcoav@users.noreply.github.com>
2021-05-28 17:51:27 -0500

    Lista de clientes registrados en wms

--- src/components/Movimientos/PaquetesColegio.vue
+++ src/components/Movimientos/PaquetesColegio.vue
@@ -1,73 +1,85 @@
 <template>
-<wrapper>
+  <wrapper>
     <v-row>
-        <v-col cols="12" align="center" class="mb-3">
-            <h2 class="indigo--text">Nombre de cliente encontrado</h2>
-            <h4 class="caption">Ciclo seleccionado</h4>
-        </v-col>
-        <v-col cols="12">
-            <h3 class="blue--text ml-3">Nombre de paquete</h3>
-        </v-col>
-        <v-col cols="12">
-            <v-data-table :headers="headers"/>
-        </v-col>
+      <v-col cols="12" align="center" class="mb-3">
+        <h2 class="indigo--text">Nombre de cliente encontrado</h2>
+        <h4 class="caption">Ciclo seleccionado</h4>
+      </v-col>
+      <v-col cols="12">
+        <h3 class="blue--text ml-3">Nombre de paquete</h3>
+      </v-col>
+      <v-col cols="12">
+        <v-data-table :headers="headers"/>
+      </v-col>
     </v-row>
     <dialogo v-model="dialogBuscarCliente" title="Buscar cliente">
-        <v-row align="center" justify="center">
-            <v-col cols="12" sm="12" md="6" align="center">
-                <v-autocomplete v-model="colegio" :items="colegiosFiltrados" no-filter item-value="idCliente" item-text="nombre" :search-input.sync="buscar" label="Cliente"/>
-            </v-col>
-            <v-col cols="12" sm="12" md="6" align="center">
-                <v-select :items="ciclos" label="Ciclo" v-model="ciclo"/>
-            </v-col>
-        </v-row>
-        <template slot="actions">
-            <v-btn text color="primary">Buscar</v-btn>
-        </template>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="12" md="6" align="center">
+          <v-autocomplete v-model="idCliente" :items="clientes" item-value="id" item-text="nombre" @change="buscar"
+                          label="Cliente"/>
+        </v-col>
+        <v-col cols="12" sm="12" md="6" align="center">
+          <v-select :items="ciclos" label="Ciclo" v-model="ciclo"/>
+        </v-col>
+      </v-row>
+      <template slot="actions">
+        <v-btn text color="primary">Buscar</v-btn>
+      </template>
     </dialogo>
-    <v-btn color="success"  dark fab fixed bottom right @click="dialogBuscarCliente = true">
-        <v-icon>mdi-text-box-search-outline</v-icon>
+    <v-btn color="success" dark fab fixed bottom right @click="dialogBuscarCliente = true">
+      <v-icon>mdi-text-box-search-outline</v-icon>
     </v-btn>
-</wrapper>
+  </wrapper>
 </template>
 
 <script>
-    export default {
-        name: "PaquetesColegio",
-        async created () {
-            this.$loader = true
-            try {
-                let anioActual = Number(new Date().getFullYear()) // Año actual
-                for (let i = 0; i <= 3; i++) {
-                    let anio = anioActual - i
-                    this.ciclos.push(anio + '-' + (anio + 1))
-                }
-            } catch (e) {
-                this.$loader = false
-                console.log(e)
-            }
-            this.$loader = false
-        },
-        data () {
-            return {
-                colegios: [],
-                ciclo: null,
-                ciclos: [],
-                colegiosFiltrados: [],
-                dialogBuscarCliente: true,
-                headers: [
-                    { text: 'ISBN13', value: 'isbn13' },
-                    { text: 'Título', value: 'libro.isbn13' },
-                    { text: 'Editorial', value: 'libro.titulo' },
-                    { text: 'Categoría', value: 'libro.editorial.nombreEditorial' },
-                    { text: '', value: 'accions' }
+  import ClienteServices from "../../services/ClienteServices";
 
-                ],
+  export default {
+    name: "PaquetesColegio",
+    data() {
+      return {
+        idCliente: null,
+        clientes: [],
+        ciclo: null,
+        ciclos: [],
+        dialogBuscarCliente: true,
+        headers: [
+          {text: 'ISBN13', value: 'isbn13'},
+          {text: 'Título', value: 'libro.isbn13'},
+          {text: 'Editorial', value: 'libro.titulo'},
+          {text: 'Categoría', value: 'libro.editorial.nombreEditorial'},
+          {text: '', value: 'accions'}
+        ]
+      }
+    },
+    async created() {
+      this.$loader = true
+      let vm = this
+      ClienteServices.getClientes()
+        .then(clientes => (vm.clientes = clientes))
+        .then(() => (vm.$loader = false))
 
-            }
-        },
 
+      try {
+        let anioActual = Number(new Date().getFullYear()) // Año actual
+        for (let i = 0; i <= 3; i++) {
+          let anio = anioActual - i
+          this.ciclos.push(anio + '-' + (anio + 1))
+        }
+      } catch (e) {
+        this.$loader = false
+        console.log(e)
+      }
+      this.$loader = false
+    },
+    methods: {
+
+      buscar (e) {
+        console.log(e)
+      }
     }
+  }
 </script>
 
 <style scoped>
