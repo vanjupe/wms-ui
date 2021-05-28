9938c61862b66a2cff51919d6307518fbc73327e
Marco Antonio Andrade Viana <45572715+marcoav@users.noreply.github.com>
2021-05-28 18:15:13 -0500

    Busca paquetes por cliente y ciclo

--- src/components/Movimientos/PaquetesCliente.vue
+++ src/components/Movimientos/PaquetesCliente.vue
@@ -9,21 +9,20 @@
         <h3 class="blue--text ml-3">Nombre de paquete</h3>
       </v-col>
       <v-col cols="12">
-        <v-data-table :headers="headers"/>
+        <v-data-table :headers="headers" :items="paquetes" />
       </v-col>
     </v-row>
     <dialogo v-model="dialogBuscarCliente" title="Buscar cliente">
       <v-row align="center" justify="center">
         <v-col cols="12" sm="12" md="6" align="center">
-          <v-autocomplete v-model="idCliente" :items="clientes" item-value="id" item-text="nombre" @change="buscar"
-                          label="Cliente"/>
+          <v-autocomplete v-model="idCliente" :items="clientes" item-value="id" item-text="nombre" label="Cliente" />
         </v-col>
         <v-col cols="12" sm="12" md="6" align="center">
-          <v-select :items="ciclos" label="Ciclo" v-model="ciclo"/>
+          <v-select v-model="ciclo" :items="ciclos" label="Ciclo" />
         </v-col>
       </v-row>
       <template slot="actions">
-        <v-btn text color="primary">Buscar</v-btn>
+        <v-btn text color="primary" @click="buscarPaquetes">Buscar</v-btn>
       </template>
     </dialogo>
     <v-btn color="success" dark fab fixed bottom right @click="dialogBuscarCliente = true">
@@ -36,13 +35,14 @@
   import ClienteServices from "../../services/ClienteServices";
 
   export default {
-    name: "PaquetesColegio",
+    name: "PaquetesCliente",
     data() {
       return {
         idCliente: null,
         clientes: [],
         ciclo: null,
         ciclos: [],
+        paquetes: [],
         dialogBuscarCliente: true,
         headers: [
           {text: 'ISBN13', value: 'isbn13'},
@@ -74,9 +74,19 @@
       this.$loader = false
     },
     methods: {
-
-      buscar (e) {
-        console.log(e)
+      async buscarPaquetes () {
+        console.log(this.idCliente)
+        this.paquetes = []
+        this.$loader = true
+        try {
+          this.paquetes = await ClienteServices.getPaquetesPorClienteCiclo(this.idCliente, this.ciclo)
+          console.log(this.paquetes)
+        } catch (e) {
+          console.log(e)
+          // snackbar de no encontró paquetes
+        }
+        this.dialogBuscarCliente = false
+        this.$loader = false
       }
     }
   }
